6d7cd77a85fed98fffeba111eb01a0a5874305ca
PawRys-Smetek <hellbu@gmail.com>
2026-08-25 11:44:34 +0200

    myApps component

---
+++ src/components/MyApps.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import { ref, onMounted } from 'vue'
+const myApps = ref<{ name: string; href: string }[]>([])
+onMounted(() =>
+  fetch('https://raw.githubusercontent.com/PawRys/shared-assets/master/myapps.json')
+    .then((r) => r.json())
+    .then((v) => (myApps.value = v)),
+)
+</script>
+
+<template>
+  <template v-for="(app, i) in myApps" :key="app.name">
+    <span v-if="i > 0"> / </span>
+    <a :href="app.href" target="_blank">
+      {{ app.name }}
+    </a>
+  </template>
+</template>
